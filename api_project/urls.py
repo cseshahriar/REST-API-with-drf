22b9b548dbaed6e8da89dd2841db775ed7a2edc0
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-26 22:30:02 +0600

    firstapp url updated

--- api_project/urls.py
+++ api_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('firstapp.urls')),
+    path('api/firstapp/', include('firstapp.urls')),
 ]
