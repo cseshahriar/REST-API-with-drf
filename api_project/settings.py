d68fc8b498433f6a4519298f940982aba4ece048
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-26 22:04:11 +0600

    postgresql setting

--- api_project/settings.py
+++ api_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'api_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'api',
+        'USER': 'postgres',
+        'PASSWORD': '847847',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
