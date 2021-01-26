9da27820d4d67bc7cc7a9dc18e5c0160e95d8f1d
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-26 22:18:13 +0600

    created employeeView

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def employeeView(request):
+    employee = {
+        'id': 123,
+        'name': 'John',
+        'sal': 20000
+    }
+    return JsonResponse(employee)
