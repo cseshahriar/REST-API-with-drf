d1c6c267f7e4f1badc3af9eed4360cc51cf0fbe1
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-26 22:41:29 +0600

    employee model created

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=60)
+    salary = models.DecimalField(max_digits=10, decimal_places=3)
+
+    def __str__(self):
+        return self.name
