60f0ee9415dc1736b4f7b491bb40b3ed2fc4f623
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-27 20:10:29 +0600

    student list create api vew created

--- students/views.py
+++ students/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from . models import Student
 from . serializers import StudentSerializer
+from rest_framework import status
 
 
-def student_list(request):
+def student_list_create_api_view(request):
     if request.method  == 'GET':
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 
