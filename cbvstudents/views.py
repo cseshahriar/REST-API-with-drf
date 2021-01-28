068a77185af54ddbb982fe6961e8cd4fc4fce7de
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-28 19:50:11 +0600

    generics api view with mixins

--- cbvstudents/views.py
+++ cbvstudents/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from django.http import Http404
-
+from rest_framework import generics, mixins
+"""
 class StudentListCreateApiView(APIView):
     
     def get(self, request):
@@ -46,4 +47,35 @@ class StudentDetailApiView(APIView):
         student = self.get_object(pk)
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+"""
 
+""" Mixins """
+class StudentListCreateApiView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class StudentDetailApiView(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
