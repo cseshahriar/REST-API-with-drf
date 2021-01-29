522506a09799e35d28aa1f8048723f4af50ab6b8
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-29 10:46:05 +0600

    author and book model created

--- nestedapp/models.py
+++ nestedapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    ratings = models.CharField(max_length=10)
+    author = models.ForeignKey(
+        Author, related_name='books',
+        on_delete=models.CASCADE
+        )
