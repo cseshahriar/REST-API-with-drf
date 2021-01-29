105a95521a7446ec58ab54cf5af2d3a487147620
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-29 22:36:38 +0600

    api validations

--- flight_reservations/serializers.py
+++ flight_reservations/serializers.py
@@ -7,16 +7,28 @@ from . models import (
 import re
 
 
+def isFlightNumberValid(flight_number):
+    print("is_flight_number_valid")
+
+
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+        """ second level priority """
+        validators = [isFlightNumberValid]
 
     def validate_flight_number(self, flight_number):
+        """ first level priority """
         if(re.match("^[a-zA-Z0-9]*$", flight_number) == None):
             raise serializers.ValidationError(
                 "Invalid flight number. make sure alpha numeric")
         return flight_number
+
+    def validate(self, data):
+        """ third level priority """
+        print(data)
+        return data
 
 
 class PassengerSerializer(serializers.ModelSerializer):
