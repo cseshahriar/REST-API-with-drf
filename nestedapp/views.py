7c9f18413e2e980bbfb5e06e08f58bd01bcf0985
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-29 11:37:38 +0600

    author and book api view

--- nestedapp/views.py
+++ nestedapp/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from . serializers import AuthorSerializer, BookSerializer
 from . models import Author, Book
+from rest_framework import generics
 
 
+class AuthorListAPIView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookListAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
