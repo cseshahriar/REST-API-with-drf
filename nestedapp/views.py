9934b4fd4275d563d45fc5c4a8815a628369a73c
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-01-29 14:55:17 +0600

    basic authentication

--- nestedapp/views.py
+++ nestedapp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from . serializers import AuthorSerializer, BookSerializer
 from . models import Author, Book
-from rest_framework import generics
 
 class AuthorListAPIView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-   
+    # basic authentication
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class AuthorDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
@@ -16,6 +22,8 @@ class AuthorDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 class BookListAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
