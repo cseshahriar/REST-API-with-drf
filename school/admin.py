416918c243374cbff4163dc00b7e970d47ff6bc8
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-16 01:35:15 +0600

    school admin register

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, Profile, School, Klass
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Profile)
+admin.site.register(School)
+admin.site.register(Klass)
