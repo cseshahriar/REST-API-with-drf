1efa386328481c38ea7cd8e997a3f4571806e3f4
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-03-31 20:18:23 +0600

    pollsapi model created

--- pollsapi/models.py
+++ pollsapi/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Poll(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)  # fk
+    question = models.CharField(max_length=100)
+    pub_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)  #fk
+    choice_text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE) # fk
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("poll", "voted_by")
